98da08dd1e43c545c719a6ae80970b08590e1842
Dmitry Olshansky <dmitry@olshansky.me>
2019-06-18 01:41:08 +0300

    Save reversed patches - they are simpler to get

--- d-to-java/auto-port.sh
+++ d-to-java/auto-port.sh
@@ -5,3 +5,10 @@ LIST=`grep -v -P '^#' list.txt`
 dub build -b debug && ./d-to-java -I ~/dmd2/src/druntime/import \
     -I vendor/dmd/src -J vendor/dmd --out ../src/main/java/org/dlang/dmd/ \
     $LIST
+echo
+echo "==== APPLYING PATCHES ===="
+cd ..
+for p in d-to-java/all.patch ; do
+    echo "Applying patch $p"
+    patch -u -R -l -p 1 < $p
+done
